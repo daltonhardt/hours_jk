0cf4ef50a2594a657b0ab64e6b421f42182cf978
Dalton <61704761+daltonhardt@users.noreply.github.com>
2025-04-11 16:24:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+#################################################
+# Daily Work Report system using Streamlit
+# Developed for : Jair Construction, LLC
+# Author        : Dalton Hardt
+# Date          : April 2025
+# Last revision : April 2025
+#################################################
 import google.generativeai as genai
-import sys
 import streamlit as st
-from streamlit_option_menu import option_menu
-import locale
+from streamlit_js_eval import get_geolocation
+
+from geopy.geocoders import Nominatim
 import os
 from pathlib import Path
 
@@ -11,29 +18,39 @@ from tempfile import NamedTemporaryFile
 from pydub import AudioSegment
 
 import json
+from json2html import *
 import re
 
-import logging  # biblioteca para não mostrar log de mensagens na tela
+import logging  # library for not showing LOG messages on screen
 
-# para não mostrar log de mensagens na tela
+# don't show LOG messages on screen
 for name, l in logging.root.manager.loggerDict.items():
     if "streamlit" in name:
         l.disabled = True
 
+# function to get geolocation using java connection
+def get_location():
+    local = get_geolocation()
+    if local:
+        lat = local['coords']['latitude']
+        lon = local['coords']['longitude']
+        # st.text(f"Lat.: {lat}   Lon.: {lon}")
+        geolocator = Nominatim(user_agent="geo_localization")
+        location = geolocator.reverse((lat, lon), exactly_one=True)
+        return location.address if location else "Oops, address not found."
+    else:
+        return "Oops, Lat/Lon not found."
+
+# function to define the html table pattern
 def html_table_format():
-    # Get current datetime
-    current_datetime = datetime.now()
-    # Convert to string and format
-    datetime_string = current_datetime.strftime('%d-%m-%Y')
-    # Define the html table format
-    out = f"""
+    out = f'''
         <body>
             <table style="width:100%; border-collapse: collapse;">
                 <tr style="background-color:#99ff99;">
                     <th colspan="3" align="center">WORKING HOURS</th>
                 </tr>
                 <tr>
-                    <th colspan="3" align="center">Date: {datetime_string}</th>
+                    <th colspan="3" align="center">Date: date when the work was done</th>
                 </tr>
                 <tr>
                     <th colspan="3" style="padding: 0.5em 0 0.5em 0; text-align: center; border: none;">Local: </th>
@@ -45,132 +62,159 @@ def html_table_format():
                 </tr>
             </table>
         </body>
-        """
-    return out
-
-def json_format():
-    out = '''
-            {
-              "location": "place where the work was done",
-              "date": "date when the work was done",
-              "entries":
-              [
-                {"name": "worker's name", "activity": "Description of the task", "hours": X},
-                {"name": "worker's name", "activity": "Description of the task", "hours": X}
-              ],
-              "total_hours": X
-            }
         '''
     return out
 
-# @st.cache_data
-def process_audio(value, name_of_model, system_instruction, description, type):
+# function to extract complete JSON blocks
+def extract_json_blocks(text):
+    json_blocks = []
+    stack = []
+    json_start = None
 
+    for i, char in enumerate(text):
+        if char == '{':
+            stack.append(char)
+            if len(stack) == 1:  # First '{' the begin of JSON
+                json_start = i
+        elif char == '}':
+            stack.pop()
+            if len(stack) == 0 and json_start is not None:  # Last '}' the end of JSON
+                json_blocks.append(text[json_start:i + 1])
+                json_start = None
+
+    return json_blocks
+
+
+# function to extract complete JSON blocks
+def extract_html_blocks(text):
+    html_blocks = []
+    pattern = re.compile(r'<body[\s\S]*?</body>', re.IGNORECASE)
+    matches = pattern.findall(text)
+    return matches
+
+
+# function to define the JSON pattern
+def json_format():
+    out = '''
+        {
+          "location": "place where the work was done",
+          "date": "date when the work was done",
+          "entries":
+          [
+            {"name": "worker's name", "activity": "Description of the task", "hours": X},
+            {"name": "worker's name", "activity": "Description of the task", "hours": X}
+          ],
+          "total_hours": X
+        }
+        '''
+    return out
+
+# function to process the audio file
+def process_audio(value, name_of_model, system_instruction, description):
+    # define Gemini Generative AI model
     model = genai.GenerativeModel(model_name=name_of_model, system_instruction=system_instruction)
-
     if value:
         with st.spinner("Working..."):
             # audio_file = st.audio(audio_value, format='audio/wav')
-            with NamedTemporaryFile(dir='.', suffix='.wav', delete=True) as f:  #delete=False mantém o arquivo
+            with NamedTemporaryFile(dir='.', suffix='.wav', delete=True) as f:  #delete=False keep the file
                 f.write(value.getbuffer())
-                # file_name = os.path.splitext(f.name)[0]
                 file_name = f.name
                 # print('\n ==================')
                 # print('Temp file_name:', file_name)
                 audio = AudioSegment.from_file(file_name)
                 new_file = os.path.splitext(f.name)[0] +'.mp3'
                 audio.export(new_file, bitrate='128k', format='mp3')
-                # print(f'===== mp3 file (new_file): \n {new_file}')
+                # print(f'===== mp3 file exported (new_file): \n {new_file}')
 
-            # st.audio(new_file, format="audio/mpeg")  # mostrar o audio player
+            # load mp3 audio file
             audio_file = genai.upload_file(new_file)
-
             # get result from AI model
-            # print(f'===== Giving the response using Google Generative AI model: {name_of_model}')
             result = model.generate_content([audio_file, description])
-
-            # print("=====  Saida Total  =====")
+            # print("=====  Total output  =====")
             # print(result.text)
 
-            start_idx = result.text.find('```html')  # Primeira chave
-            end_idx = result.text.rfind('```\n')  # Segunda chave
+            # Extract JSONs from the text
+            json_text = result.text
+            json_matches = extract_json_blocks(json_text)
 
-            if start_idx != -1 and end_idx != -1:
-                html_text = result.text[start_idx:end_idx + 1]  # Extrai o HTML somente
-                st.html(html_text.strip("```html").strip("```"))
-
-            # st.html(result.text.strip("```html").strip("```"))
-            # for chunk in result:
-            #     st.markdown(chunk.text, unsafe_allow_html=True)
-
-            # Encontra a primeira e última ocorrência de '{' e '}'
-            start_idx = result.text.find('{')  # Primeira chave '{'
-            end_idx = result.text.rfind('}')  # Última chave '}'
-
-            if start_idx != -1 and end_idx != -1:
-                json_text = result.text[start_idx:end_idx + 1]  # Extrai o JSON puro
+            # Converts each JSON into a Python dictionary
+            json_objects = []
+            for match in json_matches:
                 try:
-                    json_data = json.loads(json_text)  # Converte string para dicionário Python
-                    # print("JSON Extraído com Sucesso:", json_data)
-
-                    try:
-                        # Convert JSON string to a Python dictionary
-                        # json_data = json_text.strip("```json").strip("```")
-                        # data_dict = json.loads(json_data)
-
-                        # Extract relevant data
-                        location = json_data["location"]
-                        data = [[entry["name"], entry["activity"], entry["hours"]] for entry in json_data["entries"]]
-                        total_hours = json_data["total_hours"]
-
-                        print("=== Location:", location)
-                        print("=== Data:", data)
-                        print("=== Total Horas:", total_hours)
-
-                    except json.JSONDecodeError as e:
-                        print("Error parsing JSON:", e)
-
+                    json_objects.append(json.loads(match))
                 except json.JSONDecodeError as e:
-                    print("Erro ao decodificar JSON:", e)
+                    print(f"Error to decode JSON: {e}")
 
-            else:
-                print("JSON não encontrado")
+            # display the result
+            # i=0
+            # for obj in json_objects:
+            #     i += 1
+            #     print(f'=== Dump JSON nr. {i}')
+            #     print(json.dumps(obj, indent=2, ensure_ascii=False))
 
+            # Extract HTMLs from the text
+            html_text = result.text
+            html_matches = extract_html_blocks(html_text)
 
-        if type == 'audio':
-            action1, action2 = st.columns(2)
-            with action1:  # download arquivo em MP3
-                with open(new_file, "rb") as f:
-                    data = f.read()
-                    st.download_button(
-                        label='Save data',
-                        data=data,
-                        file_name= "audio-hours.mp3",
-                        mime="audio/mpeg"
-                    )
+            # Converts each HTML into a Python dictionary
+            html_objects = []
+            try:
+                for i, html in enumerate(html_matches, 1):
+                    # print(f"\n=== Dump HTML nr.{i} ===\n")
+                    # print(html)
+                    st.html(html)
+            except:
+                st.html("HTML not found...")
+
+        action1, _, _, action2 = st.columns(4)
+        with action1:  # download audio file in .mp3 format
+            with open(new_file, "rb") as f:
+                data = f.read()
+                st.download_button(
+                    label='Save audio',
+                    data=data,
+                    file_name= "audio-hours.mp3",
+                    mime="audio/mpeg",
+                    icon=":material/download:"
+                )
+        with action2:  # download JSON file
+            json_string = json.dumps(json_objects, ensure_ascii=False)  #ensure_Ascii=False for UTF-8 characters
+            # st.json(json_string, expanded=True)
+            st.download_button(
+                label='Save data',
+                data=json_string,
+                file_name="work_hours.json",
+                mime="application/json",
+                icon=":material/download:"
+            )
+
 
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 
 # --- GOOGLE API initialization
-# print('Configuring apikey...')
-# os.environ['GEMINI_API_KEY'] = apikey
 # Read Gemini API-Key credentials stored in secrets.toml file
 apikey = st.secrets.google_api["apikey"]
 genai.configure(api_key=apikey)
 
 # --- LOCAL configurations
 # set the locale to Spanish (Spain)
-locale.setlocale(locale.LC_NUMERIC, 'es_ES.UTF-8')
-# get today date
-TODAY = datetime.strptime(datetime.now().strftime("%d-%m-%Y"), "%d-%m-%Y")
-# LANG = ["Português-BR", "Español-ES"]
-# SOURCE = ["Audio", "Audio file"]
+# locale.setlocale(locale.LC_NUMERIC, 'es_ES.UTF-8')
 
-output_format = html_table_format()
+# Get current datetime
+current_datetime = datetime.now()
+# Convert to string and format
+datetime_string = current_datetime.strftime('%b-%d-%Y')
+TODAY = datetime.strptime(datetime.now().strftime("%b-%d-%Y"), "%b-%d-%Y")
+# Get current location
+address = get_location()
+if 'Oops' not in address:
+    add = address.split(', ')
+    address = f'{add[0]}, {add[1]}, {add[3]}'
+
+output_html = html_table_format()
 output_json = json_format()
-DESC = (f"Show an html table using the output format {output_format}."
-        f"Always sum the total worked hours and put it in the last row of the table in bold format."
-        f"Below the table display return the data in JSON format with the following structure {output_json}")
+DESC = (f"Save the result as JSON file using the format {output_json} and always sum the total worked hours per day."
+        f"Create an html table using the format {output_html} and sum the total worked hours per day in bold format.")
 
 # Define Gemini AI instructions according to selected language
 instruction = (f"Based on what you understand from the audio, always give the answer in the same language. "
@@ -180,6 +224,7 @@ instruction = (f"Based on what you understand from the audio, always give the an
               f"the task the person has done and the hours the person has spent on it. "
               f"Describe the activity in a complete form. "
               f"If you don't understand a name, write 'not clear', but keep the record of task and hours."
+              f"If the local is not mentioned in the audio use the local {address} "
               f"Do the job with no verbosity, don't display your comments. "
               f"If the date is not mentioned in the audio use the date {TODAY} in the format '%b-%d-%Y'. ")
 
@@ -189,44 +234,19 @@ model_name = "gemini-2.0-flash"
 # model_name = "gemini-2.0-flash-Lite"
 
 # --- starting STREAMLIT
-st.set_page_config(layout="wide")
-st.header('JK Services Co.')
+# st.set_page_config(layout="wide")
+st.header('Jair Construction, LLC.')
 st.subheader('Daily Work Report')
-st.text(f'- powered by Google AI model {model_name}')
+st.text(f'- powered by Google generative AI model {model_name}')
 st.divider()
 
-# st.sidebar.markdown("# Menu")
-# # Define language to be used
-# lang = st.sidebar.selectbox("Seleccione el idioma:", LANG)
-
-# Tabs
-TAB_0 = 'Speak'
-# TAB_1 = 'Subir archivo'
-
-tab = option_menu(
-    menu_title='',
-    options=['Speak'],
-    icons=['bi bi-mic-fill','bi-filetype-mp3'],
-    menu_icon='cast',
-    orientation='horizontal',
-    default_index=0
-)
-
-if tab == TAB_0:
-    # Enter the audio
-    st.markdown("Speak normally and make sure you include the following terms:")
-    st.markdown("- :blue-background[**the name of the local**]")
-    st.markdown("- :red-background[**the names of the workers**]")
-    st.markdown("- :blue-background[**the activities developed by each worker**]")
-    st.markdown("- :red-background[**the hours spent on each activity by each worker**]")
-    st.markdown("- if no :blue-background[**date**] is mentioned the system will assume the current date/time")
-    st.markdown("Press the microphone icon bellow to start speaking and press again when finished.")
-    audio_value = st.audio_input("")
-    input_type = "audio"
-    process_audio(audio_value, model_name, instruction, DESC, input_type)
-
-# if tab == TAB_1:
-#     # Select the audio file
-#     audio_value = st.file_uploader(label='Seleccione el archivo:', type=['mp3', 'm4a', 'wav'])
-#     input_type = "file"
-#     process_audio(audio_value, model_name, DESC, input_type)
+# Enter the audio
+st.markdown("Instructions: speak normally and make sure you include the following terms:")
+st.markdown("- :blue-background[**the name of the local**], if not mentioned the system will assume your current geolocation")
+st.markdown("- :red-background[**the date**], if not mentioned the system will assume the date as Today")
+st.markdown("- :blue-background[**the names of the workers**]")
+st.markdown("- :red-background[**the activities developed by each worker**]")
+st.markdown("- :blue-background[**the hours spent on each activity by each worker**]")
+st.markdown("Press the microphone icon bellow to start speaking and press again when finished.")
+audio_value = st.audio_input("")
+process_audio(audio_value, model_name, instruction, DESC)
